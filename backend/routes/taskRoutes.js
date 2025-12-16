4bcdc805c975a820dcf15728189e118786cdcab1
Logeshkanna0406 <logeshkanna161@gmail.com>
2025-12-16 20:11:11 +0530

    Add files via upload

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/authmiddleware");
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask
+} = require("../controllers/taskController");
+
+router.post("/", authMiddleware, createTask);
+router.get("/", authMiddleware, getTasks);
+router.put("/:id", authMiddleware, updateTask);
+router.delete("/:id", authMiddleware, deleteTask);
+
+module.exports = router;
